a73451fb940db188e3d1f18ddcbd5736f2e0e261
Vincent Ducrot <v.ducrot@student.isartdigital.com>
2016-04-27 12:20:07 +0200

    Client settings scene modified

--- scripts/network/client/JoinServer.gd
+++ scripts/network/client/JoinServer.gd
@@ -1,18 +1,12 @@
 
 extends Button
 
-onready var port_node = get_tree().get_current_scene().get_node("./panel/combo_box/port_spin_box")
-onready var ip_address_node = get_tree().get_current_scene().get_node("./panel/combo_box/ip_address_line_edit")
-onready var nickname_node = get_tree().get_current_scene().get_node("./panel/combo_box/nickname_line_edit")
+
 
 func _ready():
 	pass
 
 
 func _on_join_server_button_pressed():
-	if (port_node == null || ip_address_node == null):
-		return
-	
-	get_node("/root/GlobalClient").connect_to_server(ip_address_node.get_text(), port_node.get_value())
 	
 	pass # replace with function body
