cc61572e8a42bd7c9aa6762b03469ec75b6beaed
Vincent Ducrot <v.ducrot@student.isartdigital.com>
2016-04-27 11:16:15 +0200

    Multiplayer select menu added

---
+++ scripts/network/MultiplayerSelectMenu.gd
@@ -0,0 +1,18 @@
+
+extends Control
+
+func _ready():
+
+	pass
+
+
+
+
+func _on_host_game_button_pressed():
+	get_tree().get_current_scene().queue_free()
+	get_tree().change_scene("res://scenes/network/server/ServerSettings.scn")
+
+
+func _on_join_server_button_pressed():
+	get_tree().get_current_scene().queue_free()
+	get_tree().change_scene("res://scenes/network/client/ClientSettings.scn")
