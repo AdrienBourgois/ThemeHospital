a207e3ecbd2fcd18d3b00b4325cc66d2cfab58c7
Vincent Ducrot <v.ducrot@student.isartdigital.com>
2016-04-28 18:04:42 +0200

    Useless files and folders removed

--- scripts/network/Lobby.gd
+++ scripts/network/Lobby.gd
@@ -5,18 +5,24 @@ onready var message_line_edit = get_node("./panel/chat_box/message_line_edit")
 onready var global_client = get_node("/root/GlobalClient")
 var last_messages_list_size = 0
 
+
 func _ready():
 	checkHostClient()
 	setScrollFollow()
 	set_process(true)
+
 
 func _process(delta):
 	update_chat()
 
 
 func _on_send_message_button_pressed():
-	global_client.sendPacket(message_line_edit.get_text())
-	message_line_edit.clear()
+	var message = message_line_edit.get_text()
+	
+	if (!checkForEmptyMessage(message)):
+		global_client.sendPacket(parseSpaces(message))
+		message_line_edit.clear()
+
 
 func update_chat():
 	var messagesListLabel = get_node("panel/chat_box/messages_list_label")
@@ -30,6 +36,7 @@ func update_chat():
 	
 	pass
 
+
 func checkHostClient():
 	if (global_client.getClientStates().is_host):
 		get_node("panel/combo_box").set_hidden(true)
@@ -37,8 +44,10 @@ func checkHostClient():
 		get_node("panel/clients_information_box").set_hidden(true)
 	pass
 
+
 func setScrollFollow():
 	get_node("panel/chat_box/messages_list_label").set_scroll_follow(true)
+
 
 func _on_message_line_edit_input_event( ev ):
 	if (ev.is_action_pressed("accept")):
@@ -50,3 +59,20 @@ func _on_disconnect_button_pressed():
 	get_tree().get_current_scene().queue_free()
 	get_tree().change_scene("res://scenes/network/MultiplayerSelectMenu.scn")
 	pass # replace with function body
+
+
+func checkForEmptyMessage(message):
+	if (message.empty()):
+		return true
+	
+	for character in range (message.length()):
+		if (message[character] != ' '):
+			return false
+	
+	return true
+
+
+func parseSpaces(message):
+	for character in range (message.length()):
+		if (message[character] != ' '):
+			return message.substr(character, message.length())
