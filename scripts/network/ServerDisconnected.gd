592a736c0101a2ea47265c88de2a9d2090355283
Vincent Ducrot <v.ducrot@student.isartdigital.com>
2016-05-02 17:07:32 +0200

    Script for WarningServerDisconnected scene

---
+++ scripts/network/ServerDisconnected.gd
@@ -0,0 +1,15 @@
+
+extends Control
+
+# member variables here, example:
+# var a=2
+# var b="textvar"
+
+func _ready():
+	get_node("AcceptDialog").set_hidden(false)
+
+
+func _on_AcceptDialog_confirmed():
+	get_node("/root/GlobalClient").disconnectFromServer()
+	get_tree().get_current_scene().queue_free()
+	get_tree().change_scene("res://scenes/network/MultiplayerSelectMenu.scn")
