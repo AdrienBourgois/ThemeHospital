5c4562f22a63cbe5084ed3cbe5c2502f0f59b6c2
Enzo Sounou <e.sounou@student.isartdigital.com>
2016-05-03 10:36:52 +0200

    Add folder features to loader

--- scripts/Load.gd
+++ scripts/Load.gd
@@ -2,11 +2,43 @@
 extends Node
 
 onready var game = get_node("/root/Game")
+onready var gamescn = game.get_scene()
 onready var save = get_node("/root/Save")
 onready var dir = game.dir
+onready var saves_path = "res://saves/"
+onready var filename
+onready var folder_path
 
 func _ready():
 	pass
+
+func load_player(save_number):
+	if (!check_player_folder()):
+		print("Player not found")
+		return false
+	else:
+		set_filename(save_number)
+		load_player_data()
+		print("Player founded and loaded")
+		return true
+
+func load_player_data():
+	game.file.open(folder_path + filename, game.file.READ)
+	while (!game.file.eof_reached()):
+		gamescn.player.stats.parse_json(game.file.get_line())
+	game.file.close()
+
+func set_filename(save_number):
+	if (save_number == 0):
+		filename = "Quicksave"
+	else:
+		filename = "save_" + str(save_number)
+
+func check_player_folder():
+	folder_path = saves_path + gamescn.player.stats.NAME + '/'
+	if (game.dir.dir_exists(folder_path)):
+		return true
+	return false
 
 func load_init():
 	save.check_saves()
