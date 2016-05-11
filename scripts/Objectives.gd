603061e37aaa8aa200cd797c94d70a74e4311b6d
Bastien Mazuc <b.mazuc@student.isartdigital.com>
2016-05-11 12:20:34 +0200

    minor update

--- scripts/Objectives.gd
+++ scripts/Objectives.gd
@@ -62,23 +62,23 @@ func setObjectiveComplete(val, idx):
 		objectives_complete[idx] = false
 
 func _on_Player_heal_patients_percent_change( patients_percent ):
-	var val = objectives_gui.setHealPatientsPercents(patients_percent, heal_patients_percent_goal)
-	setObjectiveComplete(val, "HEAL_PATIENTS_PERCENT")
+		var val = objectives_gui.setHealPatientsPercents(patients_percent, heal_patients_percent_goal)
+		setObjectiveComplete(val, "HEAL_PATIENTS_PERCENT")
 
 func _on_Player_hospital_value_change( hospital_value ):
-	var val = objectives_gui.setHospitalValue(hospital_value, hospital_value_goal)
-	setObjectiveComplete(val, "HOSPITAL_VALUE")
+		var val = objectives_gui.setHospitalValue(hospital_value, hospital_value_goal)
+		setObjectiveComplete(val, "HOSPITAL_VALUE")
 
 func _on_Player_money_change( money ):
-	var val = objectives_gui.setMoney(money, money_goal)
-	setObjectiveComplete(val, "MONEY")
+		var val = objectives_gui.setMoney(money, money_goal)
+		setObjectiveComplete(val, "MONEY")
 
 
 func _on_Player_reputation_change( reputation ):
-	var val = objectives_gui.setReputation(reputation, reputation_goal)
-	setObjectiveComplete(val, "REPUTATION")
+		var val = objectives_gui.setReputation(reputation, reputation_goal)
+		setObjectiveComplete(val, "REPUTATION")
 
 
 func _on_Player_heal_patients_change( heal_patients ):
-	var val = objectives_gui.setHealPatients(heal_patients, heal_patients_goal)
-	setObjectiveComplete(val, "HEAL_PATIENTS")
+		var val = objectives_gui.setHealPatients(heal_patients, heal_patients_goal)
+		setObjectiveComplete(val, "HEAL_PATIENTS")
