261abf6f51a9f4ab73b25454f677d7c34fd7dbe8
Kenny Herillard <k.herillard@student.isartdigital.com>
2016-05-12 11:00:11 +0200

    Add color into mapRessources

--- scripts/Map/MapRessources.gd
+++ scripts/Map/MapRessources.gd
@@ -1,15 +1,15 @@
 extends Node
 
-const gp_office = { "ID": 1, "NAME": "gp's office", "SIZE_MIN": 4, "RESSOURCES": [], "OBJECTS": []}
-const general_diagnosis = { "ID": 2, "NAME": "general diagnosis", "SIZE_MIN": 5, "RESSOURCES": [], "OBJECTS": []}
-const psychiatric = { "ID": 10, "NAME": "psychiatric", "SIZE_MIN": 5, "RESSOURCES": [], "OBJECTS": []}
-const pharmacy = { "ID": 11, "NAME": "pharmacy", "SIZE_MIN": 4, "RESSOURCES": [], "OBJECTS": []}
-const ward = { "ID": 12, "NAME": "ward", "SIZE_MIN": 6, "RESSOURCES": [], "OBJECTS": []}
-const inflation = { "ID": 20, "NAME": "inflation", "SIZE_MIN": 4, "RESSOURCES": [], "OBJECTS": []}
-const staff_room = { "ID": 30, "NAME": "staff room", "SIZE_MIN": 4, "RESSOURCES": [], "OBJECTS": []}
-const toilets = { "ID": 31, "NAME": "toilets", "SIZE_MIN": 4, "RESSOURCES": [], "OBJECTS": []}
+var gp_office = { "ID": 1, "NAME": "gp's office", "SIZE_MIN": 4, "COLOR": colors.red, "RESSOURCES": [], "OBJECTS": []}
+var general_diagnosis = { "ID": 2, "NAME": "general diagnosis", "SIZE_MIN": 5, "COLOR": colors.white, "RESSOURCES": [], "OBJECTS": []}
+var psychiatric = { "ID": 10, "NAME": "psychiatric", "SIZE_MIN": 5, "COLOR": colors.red, "RESSOURCES": [], "OBJECTS": []}
+var pharmacy = { "ID": 11, "NAME": "pharmacy", "SIZE_MIN": 4, "COLOR": colors.pink, "RESSOURCES": [], "OBJECTS": []}
+var ward = { "ID": 12, "NAME": "ward", "SIZE_MIN": 6, "COLOR": colors.white, "RESSOURCES": [], "OBJECTS": []}
+var inflation = { "ID": 20, "NAME": "inflation", "SIZE_MIN": 4, "COLOR": colors.purple, "RESSOURCES": [], "OBJECTS": []}
+var staff_room = { "ID": 30, "NAME": "staff room", "SIZE_MIN": 4, "COLOR": colors.purple, "RESSOURCES": [], "OBJECTS": []}
+var toilets = { "ID": 31, "NAME": "toilets", "SIZE_MIN": 4, "COLOR": colors.white, "RESSOURCES": [], "OBJECTS": []}
 
-const diagnosis_rooms = {"GP_OFFICE": gp_office, "GENERAL_DIAGNOSIS": general_diagnosis}
-const treatment_rooms = {"PSYCHIATRIC": psychiatric, "PHARMACY": pharmacy, "WARD": ward}
-const clinics = {"INFLATION": inflation}
-const facilities = {"STAFF_ROOM": staff_room, "TOILETS": toilets}
+var diagnosis_rooms = {"GP_OFFICE": gp_office, "GENERAL_DIAGNOSIS": general_diagnosis}
+var treatment_rooms = {"PSYCHIATRIC": psychiatric, "PHARMACY": pharmacy, "WARD": ward}
+var clinics = {"INFLATION": inflation}
+var facilities = {"STAFF_ROOM": staff_room, "TOILETS": toilets}
