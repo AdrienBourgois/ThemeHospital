b071e7e0974fee4aec5f320157b39f03845a9fd2
Enzo Sounou <e.sounou@student.isartdigital.com>
2016-05-12 16:28:15 +0200

    Update corridor menu features

--- scripts/GUI/CorridorItemsMenu.gd
+++ scripts/GUI/CorridorItemsMenu.gd
@@ -10,6 +10,7 @@ onready var fire = panel.get_node("Fire")
 
 onready var drinkscn = preload("res://scenes/Entities/Objects/Object.scn")
 
+var items_count_array = []
 var drink_array = []
 var items_to_buy = {
 BENCH = 0,
@@ -22,28 +23,19 @@ FIRE = 0
 func _ready():
 	self.hide()
 
-func updateValues(node):
-	pass
+func updateValues():
+	items_count_array.clear()
+	items_count_array.append(bench.getValue())
+	items_count_array.append(plant.getValue())
+	items_count_array.append(radiator.getValue())
+	items_count_array.append(drink.getValue())
+	items_count_array.append(fire.getValue())
 
 func countString(value):
 	var count_string = "Count : " + str(value)
 
 func _on_Accept_pressed():
-	var node = drinkscn.instance()
-	add_child(node)
-	self.hide()
-
-func checkIsEmpty():
-	pass
-	
-func less(value):
-	value -= 1
-	if (value < 0):
-		value = 0
-	return value
+	updateValues()
 	
 func _on_Close_pressed():
 	self.hide()
-
-func _on_Drink_pressed():
-	_on_PlusDrink_pressed()
