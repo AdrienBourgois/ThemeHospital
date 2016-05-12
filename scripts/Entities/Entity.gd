70f2cc9044684eb34a77598a836d8856466dcb68
Adrien Bourgois <a.bourgois@student.isartdigital.com>
2016-05-12 15:34:57 +0200

    Fix after merge

--- scripts/Entities/Entity.gd
+++ scripts/Entities/Entity.gd
@@ -3,19 +3,20 @@ extends KinematicBody
 
 onready var game = get_node("/root/Game")
 onready var map = game.scene.map
-onready var mouse_pos_3d = map.mouse_pos
+onready var mouse_pos_3d = map.center_tile_on_cursor
 
 var is_selected = false
 var can_selected = true
 
 func _ready():
-	set_translation(mouse_pos_3d)
+	var cube_scale = get_node("TestCube").get_scale()
+	set_translation(Vector3(mouse_pos_3d.x - (cube_scale.x * 2), cube_scale.y, mouse_pos_3d.y - (cube_scale.z * 2)))
 	set_process(true)
 
 func _process(delta):
 	if can_selected == true:
-		mouse_pos_3d = map.mouse_pos
-		set_translation(mouse_pos_3d)
+		mouse_pos_3d = map.center_tile_on_cursor
+		set_translation(Vector3(mouse_pos_3d.x, 0.25, mouse_pos_3d.y))
 
 func _on_Entity_input_event( camera, event, click_pos, click_normal, shape_idx ):
 	if event.type == InputEvent.MOUSE_BUTTON && event.is_action_pressed("left_click") && can_selected == true:
