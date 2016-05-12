aee6ba873aadf2ba911c994aa01ccc95806b1bc6
Kenny Herillard <k.herillard@student.isartdigital.com>
2016-05-12 11:42:02 +0200

    Add decorations

--- scripts/Map/MapRessources.gd
+++ scripts/Map/MapRessources.gd
@@ -9,8 +9,11 @@ var ward = { "ID": 12, "NAME": "ward", "SIZE_MIN": 6, "COLOR": colors.white, "RE
 var inflation = { "ID": 20, "NAME": "inflation", "SIZE_MIN": 4, "COLOR": colors.purple, "RESSOURCES": [], "OBJECTS": []}
 var staff_room = { "ID": 30, "NAME": "staff room", "SIZE_MIN": 4, "COLOR": colors.purple, "RESSOURCES": [], "OBJECTS": []}
 var toilets = { "ID": 31, "NAME": "toilets", "SIZE_MIN": 4, "COLOR": colors.white, "RESSOURCES": [], "OBJECTS": []}
+var grass = { "ID": 40, "NAME": "grass", "COLOR": colors.green, "RESSOURCES": []}
+var pavement = { "ID": 41, "NAME": "pavement", "COLOR": colors.grey, "RESSOURCES": []}
 
 var diagnosis_rooms = {"GP_OFFICE": gp_office, "GENERAL_DIAGNOSIS": general_diagnosis}
 var treatment_rooms = {"PSYCHIATRIC": psychiatric, "PHARMACY": pharmacy, "WARD": ward}
 var clinics = {"INFLATION": inflation}
 var facilities = {"STAFF_ROOM": staff_room, "TOILETS": toilets}
+var decorations = {"GRASS": grass, "PAVEMENT": pavement }
