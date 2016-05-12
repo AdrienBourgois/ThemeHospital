b937986364df16490445688fa883040b0240a021
Kenny Herillard <k.herillard@student.isartdigital.com>
2016-05-12 16:26:34 +0200

    copy MapRessources from branch map

--- scripts/Map/MapRessources.gd
+++ scripts/Map/MapRessources.gd
@@ -9,100 +9,108 @@ var lobby = {
 }
 
 var gp_office = { 
-	"ID": 1, 
+	"ID": 1,
 	"NAME": "gp's office",
-	"SIZE_MIN": 4, 
-	"COLOR": colors.red, 
-	"RESSOURCES": {}, 
+	"SIZE_MIN": 4,
+	"COLOR": colors.red,
+	"COST": 2500,
+	"RESSOURCES": {},
 	"OBJECTS": {}
 }
 
 var general_diagnosis = { 
-	"ID": 2, 
-	"NAME": "general diagnosis", 
-	"SIZE_MIN": 5, 
-	"COLOR": colors.white, 
-	"RESSOURCES": {}, 
+	"ID": 2,
+	"NAME": "general diagnosis",
+	"SIZE_MIN": 5,
+	"COLOR": colors.white,
+	"COST": 1000,
+	"RESSOURCES": {},
 	"OBJECTS": {}
 }
 
 var psychiatric = { 
-	"ID": 10, 
-	"NAME": "psychiatric", 
-	"SIZE_MIN": 5, 
-	"COLOR": colors.red, 
+	"ID": 10,
+	"NAME": "psychiatric",
+	"SIZE_MIN": 5,
+	"COLOR": colors.red,
+	"COST": 2500,
 	"RESSOURCES": {}, 
 	"OBJECTS": {}
 }
 
 var pharmacy = { 
-	"ID": 11, 
-	"NAME": "pharmacy", 
-	"SIZE_MIN": 4, 
-	"COLOR": colors.pink, 
-	"RESSOURCES": {}, 
+	"ID": 11,
+	"NAME": "pharmacy",
+	"SIZE_MIN": 4,
+	"COLOR": colors.pink,
+	"COST": 1500,
+	"RESSOURCES": {},
 	"OBJECTS": {}
 }
 
 var ward = { 
-	"ID": 12, 
-	"NAME": "ward", 
-	"SIZE_MIN": 6, 
-	"COLOR": colors.white, 
-	"RESSOURCES": {}, 
+	"ID": 12,
+	"NAME": "ward",
+	"SIZE_MIN": 6,
+	"COLOR": colors.white,
+	"COST": 2000,
+	"RESSOURCES": {},
 	"OBJECTS": {}
 }
 
 var inflation = { 
-	"ID": 20, 
-	"NAME": "inflation", 
-	"SIZE_MIN": 4, 
-	"COLOR": colors.purple, 
-	"RESSOURCES": {}, 
+	"ID": 20,
+	"NAME": "inflation",
+	"SIZE_MIN": 4,
+	"COLOR": colors.purple,
+	"COST": 4000,
+	"RESSOURCES": {},
 	"OBJECTS": {}
 }
 
 var staff_room = { 
-	"ID": 30, 
-	"NAME": "staff room", 
-	"SIZE_MIN": 4, 
-	"COLOR": colors.purple, 
-	"RESSOURCES": {}, 
+	"ID": 30,
+	"NAME": "staff room",
+	"SIZE_MIN": 4,
+	"COLOR": colors.purple,
+	"COST": 1500,
+	"RESSOURCES": {},
 	"OBJECTS": {}
 }
 
 var toilets = {
-	"ID": 31, 
-	"NAME": "toilets", 
-	"SIZE_MIN": 4, 
-	"COLOR": colors.white, 
-	"RESSOURCES": {}, 
+	"ID": 31,
+	"NAME": "toilets",
+	"SIZE_MIN": 4,
+	"COLOR": colors.white,
+	"COST": 1500,
+	"RESSOURCES": {},
 	"OBJECTS": {}
 }
 
 var grass = {
-	"ID": 40, 
-	"NAME": "grass", 
-	"COLOR": colors.green, 
+	"ID": 40,
+	"NAME": "grass",
+	"COLOR": colors.green,
 	"RESSOURCES": {}
 }
 
 var pavement = {
-	"ID": 41, 
-	"NAME": "pavement", 
-	"COLOR": colors.grey, 
+	"ID": 41,
+	"NAME": "pavement",
+	"COLOR": colors.grey,
 	"RESSOURCES": {}
 }
 
 
 var diagnosis_rooms = {
-	"GP_OFFICE": gp_office, 
+	"GP_OFFICE": gp_office,
 	"GENERAL_DIAGNOSIS": general_diagnosis
 }
 
 var treatment_rooms = {
-	"PSYCHIATRIC": psychiatric, 
-	"PHARMACY": pharmacy, 
+	"PSYCHIATRIC": psychiatric,
+	"PHARMACY": pharmacy,
 	"WARD": ward
 }
 
@@ -111,11 +119,11 @@ var clinics = {
 }
 
 var facilities = {
-	"STAFF_ROOM": staff_room, 
+	"STAFF_ROOM": staff_room,
 	"TOILETS": toilets
 }
 
 var decorations = {
-	"GRASS": grass, 
+	"GRASS": grass,
 	"PAVEMENT": pavement 
 }
