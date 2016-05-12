8862c384675d9ed3b349e038ee88422be5b91569
Enzo Sounou <e.sounou@student.isartdigital.com>
2016-05-12 17:06:41 +0200

    Minor GUI update

--- scripts/Entities/Objects/Object.gd
+++ scripts/Entities/Objects/Object.gd
@@ -1,6 +1,8 @@
 
 extends "../Entity.gd"
 
+export var price = 100
+
 func _ready():
 	pass
 
--- scripts/GUI/CorridorItemsMenu.gd
+++ scripts/GUI/CorridorItemsMenu.gd
@@ -13,6 +13,7 @@ onready var plant_label = plant.get_node("Label")
 onready var radiator_label = radiator.get_node("Label")
 onready var drink_label = drink.get_node("Label")
 onready var fire_label = fire.get_node("Label")
+onready var label_array = [bench_label, plant_label, radiator_label, drink_label, fire_label]
 
 onready var drinkscn = preload("res://scenes/Entities/Objects/Object.scn")
 
@@ -30,11 +31,14 @@ func updateValues():
 	items_count_array.append(fire.getValue())
 
 func resetvalues():
+	var default_text = "Count : 0"
 	bench.setValue(0)
 	plant.setValue(0)
 	radiator.setValue(0)
 	drink.setValue(0)
 	fire.setValue(0)
+	for current in label_array:
+		current.set_text(default_text)
 
 func countString(value):
 	var count_string = "Count : " + str(value)
