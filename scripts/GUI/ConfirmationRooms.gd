edd7afcc5b4338e5be84c33044bcd83e04d36d49
Adrien Bourgois <a.bourgois@student.isartdigital.com>
2016-05-12 16:23:56 +0200

    Connect button to functions of room creation

--- scripts/GUI/ConfirmationRooms.gd
+++ scripts/GUI/ConfirmationRooms.gd
@@ -1,5 +1,8 @@
 extends Panel
 
+func _ready():
+	get_node("Buttons/Accept").connect("pressed", get_node("/root/Game").scene.map, "new_room", ["create", null])
+
 func _on_Cancel_pressed():
 	self.hide()
 	#######cancel all construction#########
