3f5056baaef4df679e3a6a9dde758940531bc755
Anthony De Figueiredo <a.de@student.isartdigital.com>
2016-05-13 19:12:05 +0200

    Re-add in In_game_gui, set Anchor, change select staff and object with left click to right click

--- scripts/Entities/Entity.gd
+++ scripts/Entities/Entity.gd
@@ -29,7 +29,7 @@ func _on_Entity_input_event( camera, event, click_pos, click_normal, shape_idx )
 	if event.type == InputEvent.MOUSE_BUTTON && event.is_action_pressed("left_click") && can_selected == true:
 		can_selected = false
 		set_process_input(false)
-	elif event.type == InputEvent.MOUSE_BUTTON && event.is_action_pressed("left_click") && can_selected == false:
+	elif event.type == InputEvent.MOUSE_BUTTON && event.is_action_released("right_click") && can_selected == false:
 		is_selected = true
 		can_selected = true
 		set_process_input(true)
