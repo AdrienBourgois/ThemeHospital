d40ea5a21e7b3d4294b7f2b10f252fa0b02d583c
Anthony De Figueiredo <a.de@student.isartdigital.com>
2016-05-13 12:17:29 +0200

    try to add timer to decrease or increase warmth, thirsty and happiness

--- scripts/Entities/Human/Patient.gd
+++ scripts/Entities/Human/Patient.gd
@@ -1,11 +1,35 @@
 
 extends KinematicBody
 
+export var machine = false
 var happiness
 var thirsty
 var warmth
+export var global_temperature = 0
+var count
 onready var disease = get_node("Disease")
 
 func _ready():
+	get_node("Timer").start()
+	count = 0
 	pass
 
+func _on_Timer_timeout():
+	count += 1
+	if global_temperature <= 16:
+		warmth -= 2
+	elif global_temperature >= 24:
+		warmth += 2
+	
+	if warmth <= 40 || warmth >= 60:
+		if count == 5:
+			happiness -= 2
+	
+	thirsty -= 2
+	if thirsty <= 20:
+		if count == 5:
+			happiness -= 2
+			count = 0
+		if machine == true:
+			thirsty += 50
+	print("Thirsty = ", thirsty, " | Warmth = ", warmth, " | Happiness = ", happiness)
