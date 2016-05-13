6067cb0e5c911e58fa70179fdb7543da33aa6047
Bastien Mazuc <b.mazuc@student.isartdigital.com>
2016-05-13 09:23:35 +0200

    minor update

--- scripts/Entities/Human/PatientTimerSpawn.gd
+++ scripts/Entities/Human/PatientTimerSpawn.gd
@@ -6,7 +6,6 @@ func _ready():
 
 func _on_Timer_timeout():
 	var spawn_time = rand_range(1, 20)
-	print(spawn_time)
 	set_wait_time(spawn_time)
 	get_parent().patient_array.push_back(get_parent().generatePatientData())
 	get_parent().createPatientBody()
