18884b7a3231a169f91cdb59d09fff3adfde89c0
Anthony De Figueiredo <a.de@student.isartdigital.com>
2016-05-16 16:10:05 +0200

    hide StaffGui at start and erase print.

--- scripts/GUI/SetSweepingLitterPriority.gd
+++ scripts/GUI/SetSweepingLitterPriority.gd
@@ -12,7 +12,6 @@ func _on_SweepingLitter_pressed():
 	get_parent().get_node("SweepingLitterPriority").set_val(staff_selected.sweeping_litter + 5)
 	get_parent().get_node("FixingMachineryPriority").set_val(staff_selected.fixing_machinery - 5)
 	get_parent().get_node("WateringPlantsPriority").set_val(staff_selected.watering_plants - 5)
-	print(get_parent().get_node("SweepingLitterPriority").get_val())
 	staff_selected.increaseSweepingLitter()
 	staff_selected.decreaseFixingMachinery()
 	staff_selected.decreaseWateringPlants()
