01fb71e8f19c65c50fcffab98eeef37654bc0ebd
Enzo Sounou <e.sounou@student.isartdigital.com>
2016-05-18 14:27:32 +0200

    Update object use, Minor GUI update

--- scripts/Entities/Objects/Object.gd
+++ scripts/Entities/Objects/Object.gd
@@ -6,9 +6,6 @@ export var expense_per_month = 0
 
 export var in_room_object = false
 
-func _ready():
-	pass
-
 func _on_Entity_input_event( camera, event, click_pos, click_normal, shape_idx ):
 	if event.type == InputEvent.MOUSE_BUTTON && event.is_action_pressed("left_click") && can_selected == true:
 		can_selected = false
