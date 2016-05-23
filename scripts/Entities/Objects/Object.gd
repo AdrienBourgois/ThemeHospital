3c5c25941b0cf236ca9c56288dfa055d1f83f378
Enzo Sounou <e.sounou@student.isartdigital.com>
2016-05-23 16:13:37 +0200

    Update object save

--- scripts/Entities/Objects/Object.gd
+++ scripts/Entities/Objects/Object.gd
@@ -11,21 +11,26 @@ func _on_Entity_input_event( camera, event, click_pos, click_normal, shape_idx )
 	if event.type == InputEvent.MOUSE_BUTTON && event.is_action_pressed("left_click") && can_selected == true:
 		can_selected = false
 		set_process_input(false)
-		position.x = self.get_translation().x 
-		position.y = self.get_translation().y
-		position.z = self.get_translation().z
-		var object_stats = []
-		object_stats.append(object_name)
-		object_stats.append(position.x)
-		object_stats.append(position.y)
-		object_stats.append(position.z)
-		gamescn.objects_array.append(object_stats)
+		addToArray()
+		
 	elif event.type == InputEvent.MOUSE_BUTTON && event.is_action_released("right_click") && can_selected == false:
 		if (in_room_object):
 			return
 		is_selected = true
 		can_selected = true
 		set_process_input(true)
+
+func addToArray():
+	position.x = self.get_translation().x 
+	position.y = self.get_translation().y
+	position.z = self.get_translation().z
+	
+	var object_stats = []
+	object_stats.append(object_name)
+	object_stats.append(position.x)
+	object_stats.append(position.y)
+	object_stats.append(position.z)
+	gamescn.objects_array.append(object_stats)
 
 func setName(value):
 	object_name = value
