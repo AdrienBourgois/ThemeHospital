1c3578ea8db7af25a44071d34bf0cbf8eaa272cb
Enzo Sounou <e.sounou@student.isartdigital.com>
2016-05-25 15:51:53 +0200

    Fix rooms id implementation

--- scripts/Entities/Objects/Object.gd
+++ scripts/Entities/Objects/Object.gd
@@ -12,7 +12,7 @@ var object_stats = {}
 func _on_Entity_input_event( camera, event, click_pos, click_normal, shape_idx ):
 	if event.type == InputEvent.MOUSE_BUTTON && event.is_action_pressed("left_click") && can_selected == true:
 		var type = map.columns[map.tile_on_cursor.x][map.tile_on_cursor.y].room_type
-		if (type.ID != 0 and type.ID != 40):
+		if (type.ID != 0 and type.ID != 40 and type.ID != room_id):
 			return
 		can_selected = false
 		set_process_input(false)
