2e29594978621247d5282ef863ea512d771d816e
Kenny Herillard <k.herillard@student.isartdigital.com>
2016-05-25 11:38:44 +0200

    Add value in export for tooltips on 2 button in progress bar, and add all translation at ResearchGUI

--- scripts/GUI/ResearchGUI.gd
+++ scripts/GUI/ResearchGUI.gd
@@ -2,14 +2,18 @@
 extends Control
 
 onready var container = get_node("Container")
+onready var total = get_node("Total")
 var global_value = 0
+
 
 func _ready():
 	set_global_value()
+	total.set_tooltip("TOOLTIP_RESEARCH_TOTAL")
+
 
 func set_global_value():
 	global_value = 0
 	for i in range(container.get_child_count()):
 		var value = container.get_child(i).get_value()
 		global_value += value
-		get_node("Total").set_value(global_value)
+		total.set_value(global_value)
