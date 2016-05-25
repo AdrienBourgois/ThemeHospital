a70fb3590917e923d655c7a288d417291c646a9c
Kenny Herillard <k.herillard@student.isartdigital.com>
2016-05-25 16:52:11 +0200

    Start to change diseases

---
+++ scripts/Entities/Human/DiseasesDictionaries.gd
@@ -0,0 +1,57 @@
+extends Node
+
+var invisibility = {
+	"NAME": "NAME_INVISIBILITY",
+	"DESCRIPTION": "DESC_INVISIBILITY",
+	"CAUSE": "CAUSE_INVISIBILITY",
+	"COST": 1400,
+}
+
+var bloaty_head = {
+	"NAME": "NAME_BLOATY",
+	"DESCRIPTION": "DESC_BLOATY",
+	"CAUSE": "CAUSE_BLOATY",
+	"COST": 850,
+}
+
+var tongue = {
+	"NAME": "NAME_TONGUE",
+	"DESCRIPTION": "DESC_TONGUE",
+	"CAUSE": "CAUSE_TONGUE",
+	"COST": 900,
+}
+
+var sleeping_illness = {
+	"NAME": "NAME_SLEEPING",
+	"DESCRIPTION": "DESC_SLEEPING",
+	"CAUSE": "CAUSE_SLEEPING",
+	"COST": 750,
+}
+
+var uncommon_cold = {
+	"NAME": "NAME_UNCOMMON",
+	"DESCRIPTION": "DESC_UNCOMMON",
+	"CAUSE": "CAUSE_UNCOMMON",
+	"COST": 300,
+}
+
+var TV_personalities = {
+	"NAME": "NAME_TV",
+	"DESCRIPTION": "DESC_TV",
+	"CAUSE": "CAUSE_TV",
+	"COST": 800,
+}
+
+var headed_piles = {
+	"NAME": "NAME_HEADED",
+	"DESCRIPTION": "DESC_HEADED",
+	"CAUSE": "CAUSE_HEADED",
+	"COST": 400,
+}
+
+var the_squits = {
+	"NAME": "NAME_SQUITS",
+	"DESCRIPTION": "DESC_SQUITS",
+	"CAUSE": "CAUSE_SQUITS",
+	"COST": 400,
+}
--- scripts/GUI/ResearchGUI.gd
+++ scripts/GUI/ResearchGUI.gd
@@ -8,23 +8,33 @@ onready var map = get_node("/root/Game").scene.map
 
 onready var rooms_ressources = map.ressources
 onready var rooms_type = rooms_ressources.type_rooms
+onready var clinics = rooms_type.TYPE_CLINICS
+onready var diagnosis = rooms_type.TYPE_DIAGNOSIS
 onready var research_room = rooms_type.TYPE_FACILITIES.RESEARCH
 
 var global_value = 0
+var clinics_array = []
+var diagnosis_array = []
 
 func _ready():
 	set_global_value()
+	
+	get_clinics_locked()
+	get_diagnosis_locked()
 
 func set_global_value():
 	global_value = 0
+	
 	for i in range(container.get_child_count()):
 		var value = container.get_child(i).get_value()
 		global_value += value
+		
 		total.set_value(global_value)
 
 func _on_Quit_pressed():
 	self.hide()
 	hud.show()
+	
 	set_pause_mode(false)
 
 func get_staff_in_research():
@@ -32,3 +42,17 @@ func get_staff_in_research():
 		return
 	else:
 		return research_room.STAFF
+
+func get_clinics_locked():
+	for rooms in clinics:
+		if (clinics[rooms].UNLOCK == false):
+			clinics_array.append(clinics[rooms])
+	
+	return clinics_array
+
+func get_diagnosis_locked():
+	for rooms in diagnosis:
+		if (diagnosis[rooms].UNLOCK == false):
+			diagnosis_array.append(diagnosis[rooms])
+	
+	return diagnosis_array
