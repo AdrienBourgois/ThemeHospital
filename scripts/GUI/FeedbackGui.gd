9acbb8328cb97ec919c07714ae08cd076269e342
Bastien Mazuc <b.mazuc@student.isartdigital.com>
2016-05-26 13:35:50 +0200

    fix on feedback display

--- scripts/GUI/FeedbackGui.gd
+++ scripts/GUI/FeedbackGui.gd
@@ -12,6 +12,7 @@ func _ready():
 func _input(event):
 	if event.is_action_released("left_click") && mouse_on_it == true:
 		hide()
+		mouse_on_it = false
 
 func display(txt):
 	if game.config.tutorial :
