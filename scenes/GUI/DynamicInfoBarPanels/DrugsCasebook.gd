19b35852b9e54351f92504228943ce46117e277b
Kenny Herillard <k.herillard@student.isartdigital.com>
2016-05-27 13:36:29 +0200

    Add all informations, only left the buttons

--- scenes/GUI/DynamicInfoBarPanels/DrugsCasebook.gd
+++ scenes/GUI/DynamicInfoBarPanels/DrugsCasebook.gd
@@ -6,10 +6,20 @@ onready var disease = gamescn.diseases
 onready var diseases_list = disease.list_diseases
 
 onready var hud = get_parent().get_node("HUD")
+
 onready var node_reputation = get_node("Book/Informations/Reputation/values")
+onready var node_treatment = get_node("Book/Informations/TreatmentCharge/values")
 onready var node_container = get_node("Book/Buttons/ButtonsContainer")
+onready var node_money = get_node("Book/Informations/MoneyEarned/values")
+onready var node_recoveries = get_node("Book/Informations/Recoveries/values")
+onready var node_fatalities = get_node("Book/Informations/Fatalities/values")
+onready var node_turned = get_node("Book/Informations/TurnedAway/values")
 
 onready var treatment_charge = 0
+onready var money_earned = 0
+onready var recoveries = 0
+onready var fatalities = 0
+onready var turned_away = 0
 
 func _ready():
 	connect()
@@ -21,6 +31,11 @@ func _on_Quit_pressed():
 
 func update():
 	node_reputation.set_text(str(reputation_value))
+	node_treatment.set_text(str(treatment_charge))
+	node_money.set_text(str(money_earned))
+	node_recoveries.set_text(str(recoveries))
+	node_fatalities.set_text(str(fatalities))
+	node_turned.set_text(str(turned_away))
 
 func _process(delta):
 	update()
@@ -33,8 +48,12 @@ func connect():
 			node_container.add_child(button)
 			
 			button.set_text(diseases_list[disease].NAME)
-#			buttons.connect("pressed", self, "disease_pressed",[type[rooms]])
+			button.connect("pressed", self, "disease_pressed",[diseases_list[disease]])
 			
 
-func disease_pressed():
-	pass
+func disease_pressed(disease):
+	treatment_charge = disease.COST
+	money_earned = disease.MONEY_EARNED
+	recoveries = disease.RECOVERIES
+	fatalities = disease.FATALITIES
+	turned_away = disease.TURNED_AWAY
