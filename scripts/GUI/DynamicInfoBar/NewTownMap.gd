ab254f2ce029a5101ccd44fdef02d51e40a173d1
Vincent Ducrot <v.ducrot@student.isartdigital.com>
2016-05-30 13:43:09 +0200

    Map is now automatically resized to the best resolution

--- scripts/GUI/DynamicInfoBar/NewTownMap.gd
+++ scripts/GUI/DynamicInfoBar/NewTownMap.gd
@@ -11,10 +11,10 @@ onready var heat_level_progress_bar = get_node("./Panel/HospitalManager/HeatLeve
 onready var heating_bill_label = get_node("./Panel/HospitalManager/HeatingBillLabel")
 onready var plot_manager = get_node("./Panel/PlotManager/")
 var file_map = File.new()
-var node2d = null
+var node2d = Node2D.new()
 
-var test_x = 10
-var test_y = 10
+var test_x = 0
+var test_y = 0
 var tiles = []
 
 class Tile:
@@ -23,49 +23,42 @@ class Tile:
 	var y = -1
 
 func _ready ():
-	node2d = Node2D.new()
 	checkFile()
-#	for y in range ( test_y ):
-#		for x in range ( test_x ):
-#			var tile = Tile.new()
-#			tile.x = x
-#			tile.y = y
-#			tiles.append(tile)
-#	
-	
-#	node2d.connect("draw", self, "drawMap")
-#	
-#	plot_manager.add_child(node2d)
+	node2d.connect("draw", self, "drawMap")
+	plot_manager.add_child(node2d)
+	optimizeMapSize()
 
 func checkFile():
 	file_map.open("res://Maps/map1.lvl", 1)
 	
-	var x = 0
-	var y = 0 
-	
 	if(!file_map.is_open()):
 		return
 	else:
-		test_x = file_map.get_line().to_int()
-		test_y = file_map.get_line().to_int()
-		while( !file_map.eof_reached() ):
-			var line_data = file_map.get_line()
-			
-			for car in range ( line_data.length() ):
-				var tile = Tile.new()
-				tile.x = car
-				tile.y = y
-				if (line_data[car] == "1"):
-					tile.type = "Lobby"
-				tiles.append(tile)
-			x = 0
-			y += 1
+		createMapFromFile()
 	
 	file_map.close()
+
+
+func createMapFromFile():
+	test_x = file_map.get_line().to_int()
+	test_y = file_map.get_line().to_int()
 	
-	node2d.connect("draw", self, "drawMap")
+	var x = 0
+	var y = 0 
+	var line_data
 	
-	plot_manager.add_child(node2d)
+	while( !file_map.eof_reached() ):
+		line_data = file_map.get_line()
+		
+		for car in range ( line_data.length() ):
+			var tile = Tile.new()
+			tile.x = car
+			tile.y = y
+			if (line_data[car] == "1"):
+				tile.type = "Lobby"
+			tiles.append(tile)
+		x = 0
+		y += 1
 
 func drawMap():
 	for tile in tiles:
@@ -102,14 +95,23 @@ func _on_TownMap_visibility_changed():
 	money_box.updateMoney()
 
 
-func _on_HScrollBar_value_changed( value ):
-	node2d.set_scale(Vector2(value, value))
-
+func optimizeMapSize():
+	var scale = 1
+	var new_size = 0 
+	
+	while ( scale != 20 ):
+		new_size = Vector2(test_x * scale, test_y * scale)
+		
+		if (plot_manager.get_size().x < new_size.x || plot_manager.get_size().y < new_size.y):
+			print(plot_manager.get_size())
+			print(new_size)
+			node2d.set_scale(Vector2(scale - 1, scale - 1))
+			return
+		
+		scale += 1
 
 func _on_PlotManager_input_event( ev ):
-	var pos = node2d.get_pos()
 	var size = Vector2(test_x * node2d.get_scale().x, test_y * node2d.get_scale().y)
-	
 	
 	if ( Input.is_action_pressed("left_click") ):
 		var width = size.x / test_x
