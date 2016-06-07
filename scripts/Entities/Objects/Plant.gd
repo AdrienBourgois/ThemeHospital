42c642c187c98afdd0ff8c980b39f78c389cd923
Enzo Sounou <e.sounou@student.isartdigital.com>
2016-06-07 11:55:38 +0200

    Added plant features

--- scripts/Entities/Objects/Plant.gd
+++ scripts/Entities/Objects/Plant.gd
@@ -4,12 +4,17 @@ extends "./Object.gd"
 onready var thirsty_timer = get_node("ThirstyTimer")
 
 export var radius = 5
-export var thirst = 100 setget, getThirst
+export var thirst = 100 setget getThirst, setThirst
 export var decrease_when_timout = 10
 
 func _ready():
 	thirsty_timer.connect("timeout", self, "decrease")
+	game.connect("speed_change", self, "changeWaitTime")
+	thirsty_timer.set_wait_time(30 * game.speed)
 	thirsty_timer.start()
+
+func changeWaitTime():
+	thirsty_timer.set_wait_time(30 * game.speed)
 
 func decrease():
 	thirst -= decrease_when_timout
@@ -18,3 +23,6 @@ func decrease():
 
 func getThirst():
 	return thirst
+
+func setThirst(val):
+	thirst = val
