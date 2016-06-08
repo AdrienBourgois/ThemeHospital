3afb069da3193b1178f30b0adfc4d0886c28ecac
Vincent Ducrot <v.ducrot@student.isartdigital.com>
2016-06-08 11:17:14 +0200

    Dynamic money displayer optimized. It doesn't create and destroy labels for each money update anymore

--- scripts/GUI/DynamicMoneyDisplay.gd
+++ scripts/GUI/DynamicMoneyDisplay.gd
@@ -14,7 +14,6 @@ func checkPlayerMoney():
 		updateMoney()
 
 func updateMoney():
-	destroyBoxContainer()
 	splitMoney()
 
 func splitMoney():
@@ -25,6 +24,11 @@ func splitMoney():
 	if (money == 0):
 		money_array.push_back(0)
 	else:
+		if (money < 0):
+			money_array.push_back(money%10)
+			money *= -1
+			money = money/10
+		
 		while (money != 0):
 			money_array.push_back(money%10)
 			money = money/10
@@ -32,60 +36,16 @@ func splitMoney():
 	if (money_array.size() > 7):
 		return
 	
-	money_array = reverseArray(money_array)
 	displayMoney(money_array)
 
 func displayMoney(money_array):
-	addEmptyLabels(7 - money_array.size())
-	addMoneyNumber(money_array)
-
-func addEmptyLabels(number_empty_label):
-	var label = Label.new()
-	add_child(label)
-	
-	for case in range ( number_empty_label ):
-		label = createLabelForContainer(" ")
-		
-		add_child(label)
-		add_child(VSeparator.new())
-
-func addMoneyNumber(money_array):
-	var label
-	
 	for case in range ( money_array.size() ):
 		if ( case < money_array.size() ):
-			label = createLabelForContainer(str(money_array[case]))
-		else:
-			label = createLabelForContainer(" ")
-		
-		add_child(label)
-		
-		if ( case + 1 != money_array.size()):
-			add_child(VSeparator.new())
+			for item in range ( get_child_count() ):
+				if (get_child( item ).get_name() == "Label_" + str( case )):
+					get_child( item ).set_text(str(money_array[case]))
 	
-	label = Label.new()
-	add_child(label)
-
-func createLabelForContainer(text):
-	var label = Label.new()
-	label.set_align(label.ALIGN_CENTER)
-	label.set_valign(label.VALIGN_CENTER)
-	label.set_h_size_flags(label.SIZE_EXPAND_FILL)
-	label.set_v_size_flags(label.SIZE_EXPAND_FILL)
-	label.set_text(text)
-	
-	return label
-
-
-func destroyBoxContainer():
-	for child in range ( get_child_count() ):
-		get_child(0).remove_and_skip()
-
-
-func reverseArray(array):
-	var new_array = Array()
-	
-	for position in range ( array.size() ):
-		new_array.push_back(array[array.size() - (position+1)])
-	
-	return new_array
+	for case in range (money_array.size(), 7):
+			for item in range ( get_child_count() ):
+				if (get_child( item ).get_name() == "Label_" + str( case )):
+					get_child(item).set_text(" ")
