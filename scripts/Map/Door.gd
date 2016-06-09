f71cc34e4194e20488646a181c2e434de2f9a577
Enzo Sounou <e.sounou@student.isartdigital.com>
2016-06-09 12:29:06 +0200

    Update doors pose

--- scripts/Map/Door.gd
+++ scripts/Map/Door.gd
@@ -2,6 +2,7 @@
 extends "../Entities/Objects/Object.gd"
 
 var wall
+var trans
 
 func _ready():
 	map_object = true
@@ -26,22 +27,32 @@ func _input(event):
 func poseDoor():
 	setUpItem()
 	game.feedback.display("TOOLTIP_POSEDOOR_POSED")
+	set_translation(trans)
 
 func checkWalls():
+	trans = get_translation()
 	if (tile.walls_types.Up == 1):
 		tile.walls_types.Up = 3
+		set_rotation(Vector3(0,0,0))
 		poseDoor()
+		trans.z += 0.5
 		return true
 	if (tile.walls_types.Down == 1):
 		tile.walls_types.Down = 3
+		set_rotation(Vector3(0,0,0))
 		poseDoor()
+		trans.z -= 0.5
 		return true
 	if (tile.walls_types.Left == 1):
 		tile.walls_types.Left = 3
+		set_rotation(Vector3(0,deg2rad(90),0))
+		trans.x -= 0.5
 		poseDoor()
 		return true
 	if (tile.walls_types.Right == 1):
 		tile.walls_types.Right = 3
+		set_rotation(Vector3(0,deg2rad(90),0))
+		trans.x += 0.5
 		poseDoor()
 		return true
 	game.feedback.display("TOOLTIP_POSEDOOR_ERROR")
