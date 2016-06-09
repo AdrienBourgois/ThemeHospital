ec48888b914bd2b8e138851a4b1729978260b0f0
Kenny Herillard <k.herillard@student.isartdigital.com>
2016-06-09 16:46:42 +0200

    Doctor is wandering, when a diag room is created, he go into this room and wander into this room

--- scripts/Entities/Human/State/Doctor/LookingForRoom.gd
+++ scripts/Entities/Human/State/Doctor/LookingForRoom.gd
@@ -4,9 +4,8 @@ func enter(owner):
 	owner.checkWorkRoom()
 
 func execute(owner):
-#	if owner.pathfinding.animation_completed:
-#		owner.state_machine.changeState(owner.states.waiting_for_patient)
-	pass
+	if owner.pathfinding.animation_completed == true:
+		owner.state_machine.changeState(owner.states.waiting_for_patient)
 
 func exit(owner):
 	pass
