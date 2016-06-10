78ee978aa9eac1b31b0a961b66bac6be592aeb7d
Vincent Ducrot <v.ducrot@student.isartdigital.com>
2016-06-10 12:16:38 +0200

    Town map is no longer hide or show but instanciated and destroyed instead

--- scripts/GUI/ControlPanel/DynamicInfoBar.gd
+++ scripts/GUI/ControlPanel/DynamicInfoBar.gd
@@ -8,9 +8,10 @@ onready var buttons = get_node("Buttons")
 onready var label = get_node("Label")
 
 onready var status = get_node("../../../Status")
-onready var town_map = get_node("../../../TownMap")
+#onready var town_map = get_node("../../../TownMap")
 onready var research = get_node("../../../Research")
 onready var casebook = get_node("../../../Casebook")
+onready var town_map = preload("res://scenes/GUI/DynamicInfoBarPanels/TownMap.scn")
 
 func _ready():
 	for idx in buttons.get_children():
@@ -26,7 +27,8 @@ func _on_Status_pressed():
 
 func _on_Town_Map_pressed():
 	hud.hide()
-	town_map.show()
+	game.scene.add_child(town_map.instance())
+#	town_map.show()
 	camera.pause = true
 
 func _on_Research_pressed():
