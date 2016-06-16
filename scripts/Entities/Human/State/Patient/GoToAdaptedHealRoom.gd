2ea86cccd35c5878291332e21fcc6d08914940ff
Bastien Mazuc <b.mazuc@student.isartdigital.com>
2016-06-16 10:03:15 +0200

    addpatient objective

--- scripts/Entities/Human/State/Patient/GoToAdaptedHealRoom.gd
+++ scripts/Entities/Human/State/Patient/GoToAdaptedHealRoom.gd
@@ -1,11 +1,15 @@
 
 extends State
 
+onready var gamescn = get_node("/root/Game").scene
+onready var player = gamescn.player
+
 func enter(owner):
 	owner.goToAdaptedHealRoom()
 
 func execute(owner):
 	if owner.checkEndPath():
+		player.increaseHealPatients(1)
 		owner.room_occuped.present_patient.clear()
 		owner.disease.queue_free()
 		owner.state_machine.changeState(owner.states.go_out)
