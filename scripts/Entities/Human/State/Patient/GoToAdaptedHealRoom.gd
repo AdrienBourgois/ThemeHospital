0cdf2be0402705832fe35dbf613ab6725fc2160b
Kenny Herillard <k.herillard@student.isartdigital.com>
2016-06-17 10:13:39 +0200

    Change color of 3D label, add a feedback, and change the cost which was display on the label, that wasn't the right cost, and now when a patient is heal, I put the money_earned to her new value

--- scripts/Entities/Human/State/Patient/GoToAdaptedHealRoom.gd
+++ scripts/Entities/Human/State/Patient/GoToAdaptedHealRoom.gd
@@ -10,12 +10,16 @@ func enter(owner):
 func execute(owner):
 	if owner.checkEndPath():
 		player.increaseHealPatients(1)
-		var cost = owner.disease.disease_type.DEFAULT_COST
+		var cost = owner.disease.disease_type.NEW_COST
 		player.increaseMoney(cost)
 		var label = gamescn.in_game_gui.label_3d.instance()
 		gamescn.in_game_gui.add_child(label)
 		label.setPosition(owner.get_translation() + Vector3(0,2,0))
 		label.display(str(cost) + " $")
+		print("disease type : ", owner.disease.disease_type)
+		print("money b4 : ", owner.disease.disease_type.MONEY_EARNED)
+		owner.disease.disease_type.MONEY_EARNED += owner.disease.disease_type.NEW_COST
+		print("money after : ", owner.disease.disease_type.MONEY_EARNED)
 		owner.room_occuped.present_patient.clear()
 		owner.disease.queue_free()
 		owner.state_machine.changeState(owner.states.go_out)
