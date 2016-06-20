6cf7af622fcb44417717d19933f68849936c026b
Vincent Ducrot <v.ducrot@student.isartdigital.com>
2016-06-20 11:53:52 +0200

    Staff gui is now instanciated and freed instead of being hided and showed

--- scripts/GUI/ConfirmationSack.gd
+++ scripts/GUI/ConfirmationSack.gd
@@ -3,6 +3,7 @@ extends ConfirmationDialog
 
 onready var hire_manager = get_node("/root/Game").scene.hire_manager
 onready var player = get_node("/root/Game").scene.player
+onready var root = get_tree().get_current_scene().get_node("./Control")
 
 func _ready():
 	get_cancel().connect("pressed", self, "_on_CancelButton_pressed")
@@ -11,6 +12,7 @@ func _on_ConfirmationDialog_confirmed():
 	player.decreaseMoney(hire_manager.staff_selected.salary)
 	player.decreaseExpense(hire_manager.staff_selected.salary)
 	hire_manager.sackStaff()
+	root.queue_free()
 
 func _on_CancelButton_pressed():
 	get_parent().get_parent().show()
--- scripts/GUI/ExitStaffGui.gd
+++ scripts/GUI/ExitStaffGui.gd
@@ -2,8 +2,10 @@
 extends Button
 
 onready var viewport = get_parent().get_node("Viewport")
+onready var root = get_tree().get_current_scene().get_node("Control")
 
 func _on_ExitStaffGui_pressed():
-	get_parent().hide()
-	viewport.set_process(false)
-	viewport.node = null
+#	get_parent().hide()
+#	viewport.set_process(false)
+#	viewport.node = null
+	root.queue_free()
