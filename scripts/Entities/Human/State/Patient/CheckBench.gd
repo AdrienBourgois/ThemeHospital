ce7bf011d10aa5a2bc4434eb2a156aaebe0c7e21
Anthony De Figueiredo <a.de@student.isartdigital.com>
2016-06-22 15:42:13 +0200

    fix bug on bench.

--- scripts/Entities/Human/State/Patient/CheckBench.gd
+++ scripts/Entities/Human/State/Patient/CheckBench.gd
@@ -10,10 +10,12 @@ func execute(owner):
 		owner.pathfinding.free()
 		if owner.is_diagnosed == false:
 			owner.state_machine.changeState(owner.states.go_to_gp_office)
-			owner.object_ptr.is_occuped = false
+			if owner.object_ptr:
+				owner.object_ptr.is_occuped = false
 		else:
 			owner.state_machine.changeState(owner.states.go_to_adapted_heal_room)
-			owner.object_ptr.is_occuped = false
+			if owner.object_ptr:
+				owner.object_ptr.is_occuped = false
 
 func exit(owner):
 	if owner.object_ptr:
